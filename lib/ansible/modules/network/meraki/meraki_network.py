74208a3a1e18092e7206a7aedd930e0cbeba4ec2
Kevin Breit <kevin.breit@kevinbreit.net>
2019-03-08 22:40:54 -0600

    Unchanged requests now return the original data

--- lib/ansible/modules/network/meraki/meraki_network.py
+++ lib/ansible/modules/network/meraki/meraki_network.py
@@ -296,8 +296,8 @@ def main():
                     meraki.result['data'] = r
                     meraki.result['changed'] = True
             else:
+
                 net = meraki.get_net(meraki.params['org_name'], meraki.params['net_name'], data=nets)
-                # meraki.fail_json(msg="HERE", net=net, payload=payload)
                 if meraki.is_update_required(net, payload):
                     path = meraki.construct_path('update',
                                                  net_id=meraki.get_net_id(net_name=meraki.params['net_name'], data=nets)
@@ -308,6 +308,8 @@ def main():
                     if meraki.status == 200:
                         meraki.result['data'] = r
                         meraki.result['changed'] = True
+                else:
+                    meraki.result['data'] = net
     elif meraki.params['state'] == 'absent':
         if is_net_valid(meraki, meraki.params['net_name'], nets) is True:
             net_id = meraki.get_net_id(net_name=meraki.params['net_name'],
