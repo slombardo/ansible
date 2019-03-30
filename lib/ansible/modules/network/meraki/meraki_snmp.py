851bbcf3440625c18b7ad8383cc281f4865cb3f9
Kevin Breit <kevin.breit@kevinbreit.net>
2019-03-29 20:03:02 -0500

    Add support for check mode

--- lib/ansible/modules/network/meraki/meraki_snmp.py
+++ lib/ansible/modules/network/meraki/meraki_snmp.py
@@ -203,12 +203,19 @@ def set_snmp(meraki, org_id):
     snmp = get_snmp(meraki, org_id)
     ignored_parameters = ('v3AuthPass', 'v3PrivPass', 'hostname', 'port', 'v2CommunityString', 'v3User')
     if meraki.is_update_required(snmp, full_compare, optional_ignore=ignored_parameters):
+        if meraki.module.check_mode is True:
+            meraki.result['data'] = snmp.update(payload)
+            meraki.exit_json(**meraki.result)
         r = meraki.request(path,
                            method='PUT',
                            payload=json.dumps(payload))
         if meraki.status == 200:
             meraki.result['changed'] = True
             return r
+    else:
+        if meraki.module.check_mode is True:
+            meraki.result['data'] = snmp.update(payload)
+            meraki.exit_json(**meraki.result)
     return -1
 
 
@@ -261,9 +268,6 @@ def main():
     # if the user is working with this module in only check mode we do not
     # want to make any changes to the environment, just return the current
     # state with no modifications
-    # FIXME: Work with Meraki so they can implement a check mode
-    if module.check_mode:
-        meraki.exit_json(**meraki.result)
 
     # execute checks for argument completeness
 
@@ -276,10 +280,10 @@ def main():
     org_id = meraki.params['org_id']
     if org_id is None:
         org_id = meraki.get_org_id(meraki.params['org_name'])
-
     if meraki.params['state'] == 'query':
         meraki.result['data'] = get_snmp(meraki, org_id)
     elif meraki.params['state'] == 'present':
+
         meraki.result['data'] = set_snmp(meraki, org_id)
 
     # in the event of a successful module execution, you will want to
